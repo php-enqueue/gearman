cee3f7db46c3f12a84a98d89d510ce40b0dd5bb0
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-06-27 18:39:37 +0300

    [gearman] add tests.

---
+++ Tests/GearmanDestinationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Enqueue\Gearman\Tests;
+
+use Enqueue\Gearman\GearmanDestination;
+use Enqueue\Psr\PsrQueue;
+use Enqueue\Psr\PsrTopic;
+use Enqueue\Test\ClassExtensionTrait;
+use PHPUnit\Framework\TestCase;
+
+class GearmanDestinationTest extends TestCase
+{
+    use ClassExtensionTrait;
+
+    public function testShouldImplementPsrQueueInterface()
+    {
+        $this->assertClassImplements(PsrQueue::class, GearmanDestination::class);
+    }
+
+    public function testShouldImplementPsrTopicInterface()
+    {
+        $this->assertClassImplements(PsrTopic::class, GearmanDestination::class);
+    }
+
+    public function testShouldAllowGetNameSetInConstructor()
+    {
+        $destination = new GearmanDestination('theDestinationName');
+
+        $this->assertSame('theDestinationName', $destination->getName());
+    }
+}
